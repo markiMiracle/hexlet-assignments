df7963cd923ed7a9cdc4fdb745db8ee9985d158a
@hexlet/cli <support@hexlet.io>
2024-05-25 22:19:35 +0300

    submit java-web-ru/post-form

---
+++ java-web-ru/post-form/src/main/java/exercise/App.java
@@ -0,0 +1,58 @@
+package exercise;
+
+import io.javalin.Javalin;
+import java.util.List;
+import static io.javalin.rendering.template.TemplateUtil.model;
+
+import io.javalin.http.NotFoundResponse;
+import io.javalin.rendering.template.JavalinJte;
+import exercise.model.User;
+import exercise.dto.users.UsersPage;
+import exercise.repository.UserRepository;
+import org.apache.commons.lang3.StringUtils;
+import exercise.util.Security;
+import org.eclipse.jetty.util.security.SecurityUtils;
+
+public final class App {
+
+    public static Javalin getApp() {
+
+        var app = Javalin.create(config -> {
+            config.bundledPlugins.enableDevLogging();
+            config.fileRenderer(new JavalinJte());
+        });
+
+        app.get("/", ctx -> {
+            ctx.render("index.jte");
+        });
+
+        app.get("/users", ctx -> {
+            List<User> users = UserRepository.getEntities();
+            var page = new UsersPage(users);
+            ctx.render("users/index.jte", model("page", page));
+        });
+
+        app.get("/users/build", ctx -> {
+            ctx.render("users/build.jte");
+        });
+        app.post("/users", ctx -> {
+            var firstName = StringUtils.capitalize(ctx.formParam("firstName"));
+            var lastName = StringUtils.capitalize(ctx.formParam("lastName"));
+            var email = ctx.formParam("email").toLowerCase().trim();
+            var password = ctx.formParam("password");
+            password = Security.encrypt(password);
+            var user = new User(firstName, lastName, email, password);
+            UserRepository.save(user);
+
+            ctx.redirect("/users");
+        });
+
+
+        return app;
+    }
+
+    public static void main(String[] args) {
+        Javalin app = getApp();
+        app.start(7070);
+    }
+}
