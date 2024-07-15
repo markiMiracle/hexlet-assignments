774363eda8e7817abb9a3ace1da8d1b5011dedc8
@hexlet/cli <support@hexlet.io>
2024-07-16 00:30:37 +0300

    submit java-spring-ru/jpa-comparing-entities

---
+++ java-spring-ru/jpa-comparing-entities/src/main/java/exercise/exception/ResourceAlreadyExistsException.java
@@ -0,0 +1,10 @@
+package exercise.exception;
+
+
+
+
+public class ResourceAlreadyExistsException extends RuntimeException {
+    public ResourceAlreadyExistsException(String message) {
+        super(message);
+    }
+}
